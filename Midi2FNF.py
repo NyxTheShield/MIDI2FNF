5fd2fb16f8ad239a152e6190d593c099c64f1c5d
NyxTheShield <felipe.montero.13@sansano.usm.cl>
2021-03-23 00:52:52 -0300

    Fixed Parsing Issues

--- Midi2FNF.py
+++ Midi2FNF.py
@@ -4,6 +4,7 @@ import json
 import random
 from easygui import *
 import os
+import math
 
 DEFAULT_TEMPO = 0.5
 
@@ -20,6 +21,20 @@ def note2freq(x):
     """
     a = 440
     return (a/32) * (2 ** ((x-9)/12))
+
+def round_decimals_up(number:float, decimals:int=2):
+    """
+    Returns a value rounded up to a specific number of decimal places.
+    """
+    if not isinstance(decimals, int):
+        raise TypeError("decimal places must be an integer")
+    elif decimals < 0:
+        raise ValueError("decimal places has to be 0 or more")
+    elif decimals == 0:
+        return math.ceil(number)
+
+    factor = 10 ** decimals
+    return math.ceil(number * factor) / factor
 
 path = fileopenbox()
 filename = os.path.basename(path)
@@ -73,102 +88,106 @@ if __name__ == '__main__':
                     print("Unsupported metamessage: " + str(message))
 
             else:  # Note
-                if message.type == "control_change" or \
-                   message.type == "program_change":
-                    pass
-
-                elif message.type == "note_on" or message.type == "note_off":
-                    if message.note not in notes:
-                        notes[message.note] = 0
-                    if message.type == "note_on" and message.velocity != 0:
-                        notes[message.note] += 1
-                        if(notes[message.note] == 1):
-                            tracksMerged += \
-                                [(totaltime, message.note, message.velocity)]
-
-                    else:
-                        notes[message.note] -= 1
-                        if(notes[message.note] == 0):
-                            tracksMerged += \
-                                [(totaltime, message.note, message.velocity)]
-                    
                 
                 globalTime+= message.time
                 if (message.type == "note_on"):
                     currTime = globalTime*tick_duration*1000
                     noteToUse = 0
-                    if (currTrack == 1):
+                    if (message.channel == 0):
+                        #print(message)
                         if (message.note < 60 or message.note> 63):
                             noteToUse = random.choice([0,1,2,3])
-                        else:
+                        else: 
                             noteToUse = message.note-60
-                    if (currTrack == 2):
+                    if (message.channel == 1):
+                        #print(message)
                         if (message.note < 72 or message.note> 75):
                             noteToUse = random.choice([4,5,6,7])
                         else:
                             noteToUse = message.note-72+4     
                     aux = [currTime,noteToUse,0]
                     #print(aux)
-                    nyxTracks[currTrack] += [aux]
+                    nyxTracks[message.channel] += [aux]
 
         #print("totaltime: " + str(totaltime)+"s")
 
     frames = []
     currTime = 240/bpm
+    tolerance = (240/bpm)/32;
     while currTime < totaltime:
         #print("FRAME!!" + str(currTime))
         aux = []
+        for note in list(nyxTracks[0]):
+            roundedNote = round_decimals_up(note[0],3)
+            if roundedNote + tolerance < currTime*1000:
+                #print("track0")
+                #print(note)
+                aux+=[ [roundedNote ,note[1],note[2]] ]
+                nyxTracks[0].remove(note)
+                
         for note in list(nyxTracks[1]):
-            if note[0]< currTime*1000:
-                print("track1")
-                print(note)
-                aux+=[note]
+            roundedNote = round_decimals_up(note[0],3)
+            if roundedNote + tolerance < currTime*1000:
+                #print("track0")
+                #print(note)
+                aux+=[ [roundedNote ,note[1],note[2]] ]
                 nyxTracks[1].remove(note)
+
+        
         frames+=[aux]
 
-        aux = []
-        for note in list(nyxTracks[2]):
-            if note[0]< currTime*1000:
-                print("track2")
-                print(note)
-                aux+=[note]
-                nyxTracks[2].remove(note)
-        frames+=[aux]
+        print("Notes on this frame:")
+        for x in aux:
+            print(x)
         
         currTime += 240/bpm
 
+    msg = "Enter the Chart Info"
+    title = "Chart Info"
+    fieldNames = ["Songname","Stage","Player1 Character","Player2 Character"]
+    fieldValues = [songName, "stage", "bf", "dad"]  # we start with blanks for the values
+    fieldValues = multenterbox(msg,title, fieldNames, fieldValues)
+
+    # make sure that none of the fields was left blank
+    while 1:
+        if fieldValues == None: break
+        errmsg = ""
+        for i in range(len(fieldNames)):
+          if fieldValues[i].strip() == "":
+            errmsg = errmsg + ('"%s" is a required field.\n\n' % fieldNames[i])
+        if errmsg == "": break # no problems found
+        fieldValues = multenterbox(errmsg, title, fieldNames, fieldValues)
+     
 
     dicc = dict()
     dicc["song"] = {}
-    dicc["song"]["player1"] = "bf"
-    dicc["song"]["player2"] = "dad"
+    dicc["song"]["player1"] = fieldValues[2]
+    dicc["song"]["player2"] = fieldValues[3]
     dicc["song"]["notes"] = []
     dicc["song"]["isHey"] = False
     dicc["song"]["cutsceneType"] = "none"
-    dicc["song"]["song"]=songName
+    dicc["song"]["song"]= fieldValues[0]
     dicc["song"]["isSpooky"]=False
     dicc["song"]["validScore"]=True
     dicc["song"]["speed"]=2
     dicc["song"]["isMoody"]=False
     dicc["song"]["sectionLengths"]=[]
     dicc["song"]["uiType"]="normal"
-    dicc["song"]["stage"]="stage"
+    dicc["song"]["stage"]=fieldValues[1]
     dicc["song"]["sections"]=0
     dicc["song"]["needsVoices"]=True
     dicc["song"]["bpm"]=bpm
     dicc["song"]["gf"]="gf"
 
     for i, notes in enumerate(frames):
-        #print("asdf")
-        #print(notes)
-        auxDicc = dict()
-        auxDicc["typeOfSection"]=0
-        auxDicc["lengthInSteps"]=16
-        auxDicc["sectionNotes"]=notes
-        auxDicc["altAnim"]=False
-        auxDicc["mustHitSection"]=True
-        auxDicc["bpm"]=bpm
-        dicc["song"]["notes"]+=[auxDicc]
+            auxDicc = dict()
+            auxDicc["typeOfSection"]=0
+            auxDicc["lengthInSteps"]=16
+            auxDicc["sectionNotes"]=notes
+            auxDicc["altAnim"]=False
+            auxDicc["mustHitSection"]=True
+            auxDicc["bpm"]=bpm
+            dicc["song"]["notes"]+=[auxDicc]
     json = json.dumps(dicc)
 
     out = filesavebox(default=filename+'.json')
